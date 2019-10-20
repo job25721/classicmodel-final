02a992ffe44160ff63b6cea1e6b103a5f748c001
ElevaterCPE26 <oipz2001@gmail.com>
2019-10-20 19:05:09 +0700

    --

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 module.exports = mysql.createConnection({
     host : 'localhost',
     user: 'root',
-    password: '',
+    password: '054613100',
     database: 'classicmodels'
 })
 
